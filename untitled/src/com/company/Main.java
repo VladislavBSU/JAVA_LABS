4609bf04581a99a986bd8482df3ce825d9b3cd23
VladislavBSU <vlados_phizikos@mail.ru>
2018-09-17 09:51:16 +0400

    Добавление 1 и 2 лабораторной

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.lang.String;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner (System.in);
+        String s=in.next();
+        System.out.println(s);
+        Map<Integer,String> map = new HashMap<>();
+        map.get()
+        int l=Integer.parseInt(s);
+        for(int i=0; i<l ;i++){
+        String m=s.substring( i, 1);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.
+	// write your code here
+    }
+}
